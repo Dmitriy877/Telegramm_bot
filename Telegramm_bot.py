cfb5f9b4191c9a38948facf48e6a050ba5111ca9
Dmitriy877 <dmitriybukin05@gmail.com>
2024-11-28 19:19:18 +0300

    Update Telegramm_bot.py

--- Telegramm_bot.py
+++ Telegramm_bot.py
@@ -3,7 +3,6 @@ import os
 import ptbot
 from dotenv import load_dotenv
 from pytimeparse import parse
-load_dotenv()
 
 def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
     iteration = min(total, iteration)
@@ -13,24 +12,26 @@ def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='
     pbar = fill * filled_length + zfill * (length - filled_length)
     return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
 
-def choose(author_id, message, bot_choose):
+def choose(author_id, bot_choose):
     answer = 'Время вышло'
     bot_choose.send_message(author_id, answer)
 
 def notify_progress(secs_left, author_id, message_id, question, bot_notify):
     answer = "Осталось секунд {0}\n{1}".format(secs_left, render_progressbar(parse(question), secs_left))
-    bot_notify.update_message(os.environ['TG_CHAT_ID'], message_id, answer)
+    bot_notify.update_message(author_id, message_id, answer)
 
 def wait(chat_id, question, bot_wait):
-    message_id = bot_wait.send_message(os.environ['TG_CHAT_ID'], "Таймер запущен")
-    bot_wait.create_timer(parse(question), choose, author_id=chat_id, message=question, bot_choose=bot_wait)
+    message_id = bot_wait.send_message(chat_id, "Таймер запущен")
+    bot_wait.create_timer(parse(question), choose, author_id=chat_id, bot_choose=bot_wait)
     bot_wait.create_countdown(parse(question), notify_progress, author_id=chat_id, message_id=message_id, question=question, bot_notify=bot_wait)
 
 def main():
 
+    load_dotenv()
+
     bot = ptbot.Bot(os.environ['TG_TOKEN'])
 
-    bot.reply_on_message(wait, bot_wait = bot)
+    bot.reply_on_message(wait, bot_wait=bot)
 
     bot.run_bot()
 
